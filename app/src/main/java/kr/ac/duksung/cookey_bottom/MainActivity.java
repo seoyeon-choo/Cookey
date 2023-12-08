de166266c4b8318ea6d86ddbd636ab6782e57c8e
jiyeong <145409157+ziizero@users.noreply.github.com>
2023-12-08 15:11:26 +0900

    Update MainActivity.java

--- app/src/main/java/kr/ac/duksung/cookey_bottom/MainActivity.java
+++ app/src/main/java/kr/ac/duksung/cookey_bottom/MainActivity.java
@@ -1,11 +1,19 @@
 package kr.ac.duksung.cookey_bottom;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.CalendarView;
 import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import android.widget.TextView;
 import android.view.MenuItem;
@@ -13,7 +21,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.text.SimpleDateFormat;
@@ -25,9 +32,15 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private TextView timeTextView;
+    private TextView progressTextView;
+    private Handler handler;
     private RecyclerView recyclerView;
     private BottomNavigationView bottomNavigationView;
-    private FloatingActionButton addButton;
+    private NotificationManager notificationManager;
+    // Moved NOTIFICATION_ID initialization to a more suitable location
+    private static final String CHANNEL_ID = "channel_id";
+    private static final String CHANNEL_NAME = "Channel Name";
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
         CalendarView calendarView = findViewById(R.id.calendarView);
         timeTextView = findViewById(R.id.timeTextView);
+        progressTextView = findViewById(R.id.progressTextView);
 
-        // Set the header text color programmatically
         setCalendarHeaderTextColor(calendarView, android.R.color.primary_text_light);
 
-        // Display the current time
         displayCurrentTime();
 
         List<FrequentlyItem> dataList = new ArrayList<>();
@@ -49,14 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
         FrequentlyAdapter adapter = new FrequentlyAdapter(dataList);
 
-// 추가된 코드: recyclerView 초기화
         recyclerView = findViewById(R.id.recyclerView);
-
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         recyclerView.setAdapter(adapter);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        // BottomNavigationView의 아이템 클릭 리스너 설정
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -79,8 +88,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        handler = new Handler();
+        handler.postDelayed(updateTimeRunnable, 100);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        createNotificationChannel();
     }
 
+    private int getSaturationLevel() {
+        // 여기에 실제로 포화도를 가져오는 로직을 구현합니다.
+        // 예를 들어, 냉장고 정보를 가상으로 설정하거나, 센서 값을 반환하도록 구현할 수 있습니다.
+        // 가상으로 90%를 반환하는 예시:
+        return 90;
+    }
+
+    private final Runnable updateTimeRunnable = new Runnable() {
+        @Override
+        public void run() {
+            displayCurrentTime();
+            int saturationLevel = getSaturationLevel();
+            if (saturationLevel >= 90) {
+                showNotification("냉장고를 털어주세요!", "포화도가 90%에 도달했습니다.");
+            }
+            handler.postDelayed(this, 100);
+        }
+    };
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotification(String title, String message) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.but)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+        builder.setContentIntent(pendingIntent);
+
+        // Use NOTIFICATION_ID here
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
 
     private void setCalendarHeaderTextColor(CalendarView calendarView, @ColorRes int colorRes) {
         int color = ContextCompat.getColor(this, colorRes);
@@ -94,12 +156,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayCurrentTime() {
-        // 현재 시간을 받아옴
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault()); // 24시간 형식
-        String currentTime = sdf.format(calendar.getTime());
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
-        // 받아온 현재 시간을 TextView에 표시
+        String currentTime = String.format(Locale.getDefault(), "%02d:%02d", hour, minute);
         timeTextView.setText(currentTime);
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(updateTimeRunnable);
+        super.onDestroy();
+    }
+
+    private void displayCurrentSaturation() {
+        int saturation = 75;
+        progressTextView.setText(saturation + "%");
     }
 }
