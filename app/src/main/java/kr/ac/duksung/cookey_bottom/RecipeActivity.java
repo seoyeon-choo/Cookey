94350ce89cfaeb196bb1d44a665eff4ffd762cba
seoyeong <seoyeonglee@umail.ucsb.edu>
2023-12-08 13:22:48 +0900

    RecipeActivity merge 수정

--- app/src/main/java/kr/ac/duksung/cookey_bottom/RecipeActivity.java
+++ app/src/main/java/kr/ac/duksung/cookey_bottom/RecipeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
 import android.util.Log;
